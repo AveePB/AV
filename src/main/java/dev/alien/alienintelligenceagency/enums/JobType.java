2e78293a6a12b5ba8152686cd68972ec4ba527a3
AveePB <mike303.py@gmail.com>
2023-12-08 14:55:06 +0100

    Updated application enums

---
+++ src/main/java/dev/alien/alienintelligenceagency/enums/JobType.java
@@ -0,0 +1,12 @@
+package dev.alien.alienintelligenceagency.enums;
+
+
+public enum JobType {
+    CONSTRUCTION_WORKER,
+    SHOP_ASSISTANT,
+    SCIENTIST,
+    TEACHER,
+    CLEANER,
+    WAITER,
+    NONE,
+}
---
+++ src/main/java/dev/alien/alienintelligenceagency/enums/MissionType.java
@@ -0,0 +1,11 @@
+package dev.alien.alienintelligenceagency.enums;
+
+
+public enum MissionType {
+    UNCONVENTIONAL_WARFARE,
+    DIRECT_ACTION,
+    SPECIAL_RECONNAISSANCE,
+    COUNTER_TERRORISM,
+    CIVIC_ACTION,
+    NONE,
+}
