475db3290391f2d0417fd37c9f6fefd3c104df51
AveePB <mike303.py@gmail.com>
2023-12-08 13:46:01 +0100

    Clean project

---
+++ src/main/java/dev/alien/alienintelligenceagency/AlienIntelligenceAgencyApplication.java
@@ -0,0 +1,13 @@
+package dev.alien.alienintelligenceagency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienIntelligenceAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienIntelligenceAgencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alien/alienintelligenceagency/AlienIntelligenceAgencyApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alien.alienintelligenceagency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlienIntelligenceAgencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
