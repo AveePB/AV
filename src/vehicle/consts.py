047c540fdd3f301969b198ddcc17fc1f5c0cba27
AveePB <mike303.py@gmail.com>
2025-03-24 16:32:46 +0100

    Initialized packages

---
+++ src/vehicle/consts.py
@@ -0,0 +1,21 @@
+"""
+    Module responsible for storing constants used for communication with electronic components e.g. leds, motors etc.
+"""
+
+# Motor pin constants
+F_ENA = 'GPIO21'
+F_IN1 = 'GPIO20'
+F_IN2 = 'GPIO16'
+F_IN3 = 'GPIO26'
+F_IN4 = 'GPIO19'
+F_ENB = 'GPIO13'
+
+B_ENA = 'GPIO24'
+B_IN1 = 'GPIO23'
+B_IN2 = 'GPIO18'
+B_IN3 = 'GPIO22'
+B_IN4 = 'GPIO27'
+B_ENB = 'GPIO17'
+
+# Lidar USB header
+USB_HEADER = '...'
