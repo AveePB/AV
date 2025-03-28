5d1894c9e26693d25c6729747cd54cb961e035f3
AveePB <mike303.py@gmail.com>
2025-03-28 20:09:42 +0100

    Updated new maneuvers

--- src/vehicle/modules/motors.py
+++ src/vehicle/modules/motors.py
@@ -25,23 +25,35 @@ class Maneuver(Enum):
 class DCMotor(Motor):
 
     def __init__(self, forward, backward, enable):
+        """
+        Constructor responsible for correct initialization of motor.
+        """
         super().__init__(forward=forward, backward=backward, enable=enable)
 
         self.__directon = MotorDirection.NONE
 
     def moveForward(self):
+        """
+            Updates motor's spin direction to forward.
+        """
         if (self.__directon == MotorDirection.FORWARD): return
 
         self.forward(MOTOR_SPEED)
         self.__directon = MotorDirection.FORWARD
 
     def moveBackward(self):
+        """
+            Updates motor's spin direction to backward.
+        """
         if (self.__directon == MotorDirection.BACKWARD): return
 
         self.backward(MOTOR_SPEED)
         self.__directon = MotorDirection.BACKWARD
 
     def stopMovement(self):
+        """
+            Updates motor's spin direction to none.
+        """
         if (self.__directon == MotorDirection.NONE): return
 
         self.stop()
@@ -60,7 +72,22 @@ class MotorSystem:
 
         self.__maneuver = Maneuver.STOP
 
+    def stop(self):
+        """
+            Changes the motors maneuver to stop.
+        """
+        if (self.__maneuver == Maneuver.STOP): return
+
+        self.__FL_motor.stopMovement()
+        self.__FR_motor.stopMovement()
+        self.__BL_motor.stopMovement()
+        self.__BR_motor.stopMovement()
+        self.__maneuver = Maneuver.STOP
+
     def go_forward(self):
+        """
+            Changes the motors maneuver to forward.
+        """
         if (self.__maneuver == Maneuver.GO_FORWARD): return
 
         self.__FL_motor.moveForward()
@@ -68,4 +95,42 @@ class MotorSystem:
         self.__BL_motor.moveForward()
         self.__BR_motor.moveForward()
         self.__maneuver = Maneuver.GO_FORWARD
+    
+    def go_backward(self):
+        """
+            Changes the motors maneuver to backward.
+        """
+        if (self.__maneuver == Maneuver.GO_BACKWARD): return
+
+        self.__FL_motor.moveBackward()
+        self.__FR_motor.moveBackward()
+        self.__BL_motor.moveBackward()
+        self.__BR_motor.moveBackward()
+        self.__maneuver = Maneuver.GO_BACKWARD
+
+    def go_left(self):
+        """
+            Changes the motors maneuver to left.
+        """
+        if (self.__maneuver == Maneuver.GO_LEFT): return
+
+        self.__FL_motor.moveForward()
+        self.__FR_motor.moveBackward()
+        self.__BL_motor.moveBackward()
+        self.__BR_motor.moveForward()
+        self.__maneuver = Maneuver.GO_LEFT
+    
+    def go_right(self):
+        """
+            Changes the motors maneuver to right.
+        """
+        if (self.__maneuver == Maneuver.GO_RIGHT): return
+
+        self.__FL_motor.moveBackward()
+        self.__FR_motor.moveForward()
+        self.__BL_motor.moveForward()
+        self.__BR_motor.moveBackward()
+        self.__maneuver = Maneuver.GO_RIGHT
+
+    
 
--- src/vehicle/tests/test_motors.py
+++ src/vehicle/tests/test_motors.py
@@ -4,14 +4,45 @@ import time
 
 class TestMotorSystem(unittest.TestCase):
 
-    def test_go_forward(self):
+    MOVEMENT_TIME = 5 # seconds
 
+    def test_go_forward(self):
         ms = MotorSystem()
 
+        print("Move forward!")
         ms.go_forward()
-        time.sleep(4)       
 
-        print("GO FORWARD")
+        time.sleep(self.MOVEMENT_TIME)
+        print("Maneuver completed.")
+    
+    def test_go_backward(self):
+        ms = MotorSystem()
+
+        print("Move backward!")
+        ms.go_backward()
+        
+        time.sleep(self.MOVEMENT_TIME)
+        print("Maneuver completed.")
+
+    def test_go_left(self):
+        ms = MotorSystem()
+
+        print("Move left!")
+        ms.go_left()
+        
+        time.sleep(self.MOVEMENT_TIME)
+        print("Maneuver completed.")
+    
+    def test_go_right(self):
+        ms = MotorSystem()
+
+        print("Move right!")
+        ms.go_right()
+        
+        time.sleep(self.MOVEMENT_TIME)
+        print("Maneuver completed.")
+
+
 
 
 if (__name__ == '__main__'):
