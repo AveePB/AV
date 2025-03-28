c01163fdb5e622e6af332a5b1d0ac1ae5cc6fcdf
AveePB <mike303.py@gmail.com>
2025-03-28 19:46:30 +0100

    test

--- src/vehicle/consts.py
+++ src/vehicle/consts.py
@@ -3,19 +3,19 @@
 """
 
 # Motor pin constants
-F_ENA = 'GPIO21'
-F_IN1 = 'GPIO20'
-F_IN2 = 'GPIO16'
-F_IN3 = 'GPIO26'
-F_IN4 = 'GPIO19'
-F_ENB = 'GPIO13'
+F_ENA = 21 #'GPIO21'
+F_IN1 = 20 #'GPIO20'
+F_IN2 = 16 #'GPIO16'
+F_IN3 = 26 #'GPIO26'
+F_IN4 = 19 #'GPIO19'
+F_ENB = 13 #'GPIO13'
 
-B_ENA = 'GPIO24'
-B_IN1 = 'GPIO23'
-B_IN2 = 'GPIO18'
-B_IN3 = 'GPIO22'
-B_IN4 = 'GPIO27'
-B_ENB = 'GPIO17'
+B_ENA = 24 #'GPIO24'
+B_IN1 = 23 #'GPIO23'
+B_IN2 = 18 #'GPIO18'
+B_IN3 = 22 #'GPIO22'
+B_IN4 = 27 #'GPIO27'
+B_ENB = 17 #'GPIO17'
 
 # Lidar USB header
 USB_HEADER = '...'
