3b01e6fc4d020e53d2d3701a427baf9e6d7b4c18
AveePB <mike303.py@gmail.com>
2025-04-03 18:24:12 +0200

    Updated detection system

--- src/vehicle/tests/test_detection.py
+++ src/vehicle/tests/test_detection.py
@@ -6,15 +6,16 @@ import cv2
 class TestMotorSystem(unittest.TestCase):
 
     TEST_TIME = 5 # seconds
-    ds = DetectionSystem()
 
-    def test_capture_image(self):
+    def test_detection_system_scan_env(self):
+        # Initialize detection system
+        ds = DetectionSystem()
         winname = 'Camera Test'
         
         # Turn on camera
         print("Take a picture!")
         
-        img = self.ds.get_image()
+        img = ds.get_image()
         self.assertIsNotNone(img)
 
         # Create preview window
@@ -22,21 +23,15 @@ class TestMotorSystem(unittest.TestCase):
         cv2.imshow(winname, img)
         cv2.moveWindow(winname, 0, 0)
         cv2.waitKey(self.TEST_TIME * 1000) # wait x seconds
-
-        # Turn off camera
-        cv2.destroyAllWindows()  
-        print("Picture taken.")              
-
-    def test_detection_system_scan_env(self):
+        
         # Perform first scan
-        ds = DetectionSystem()
         points = ds.scan_env()
 
         # Check if total size of scan is equal
         print(points)
         self.assertEqual(len(points), LIDAR_N_SCANS*LIDAR_SCAN_SIZE)
 
-        # Turn off elements
+        # Turn off detection system
         ds.turn_off()
 
 if (__name__ == '__main__'):
