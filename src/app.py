bd9e0dcfbe162e66ebfdd9bc599663b65ca7338e
AveePB <mike303.py@gmail.com>
2025-04-14 20:42:09 +0200

    Added home route

--- src/app.py
+++ src/app.py
@@ -4,5 +4,9 @@ from flask import Flask
 app = Flask(__name__)
 app.register_blueprint(CONTROLS_BP)
 
+@app.route('/', methods=['GET'])
+def home():
+    return 'Welcome home'
+
 if (__name__ == '__main__'):
     app.run(debug=True)
