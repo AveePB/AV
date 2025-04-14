5dc9e7436db12b86049c84d5662142d861c722e9
AveePB <mike303.py@gmail.com>
2025-04-14 20:58:28 +0200

    Updated routes of the web app

--- src/controls.py
+++ src/controls.py
@@ -30,3 +30,75 @@ def stopDriving():
         return Response('Failed to stop the car!', status=500, mimetype='application/json')
     
     return Response('Successfully stopped the car!', status=200, mimetype='application/json')
+
+@CONTROLS_BP.route('/go-left', methods=['POST'])
+def driveLeft():
+    try:
+        DRIVE_SYSTEM.go_left()
+    except Exception:
+        return Response('Failed to change the direction of the car!', status=500, mimetype='application/json')
+    
+    return Response('Successfully set direction of the car to Left!', status=200, mimetype='application/json')
+
+@CONTROLS_BP.route('/go-right', methods=['POST'])
+def driveRight():
+    try:
+        DRIVE_SYSTEM.go_right()
+    except Exception:
+        return Response('Failed to change the direction of the car!', status=500, mimetype='application/json')
+    
+    return Response('Successfully set direction of the car to right!', status=200, mimetype='application/json')
+
+@CONTROLS_BP.route('/go-top-left', methods=['POST'])
+def driveTopLeft():
+    try:
+        DRIVE_SYSTEM.go_top_left()
+    except Exception:
+        return Response('Failed to change the direction of the car!', status=500, mimetype='application/json')
+    
+    return Response('Successfully set direction of the car to top left!', status=200, mimetype='application/json')
+
+@CONTROLS_BP.route('/go-top-right', methods=['POST'])
+def driveTopRight():
+    try:
+        DRIVE_SYSTEM.go_top_right()
+    except Exception:
+        return Response('Failed to change the direction of the car!', status=500, mimetype='application/json')
+    
+    return Response('Successfully set direction of the car to top right!', status=200, mimetype='application/json')
+
+@CONTROLS_BP.route('/go-bottom-left', methods=['POST'])
+def driveBottomLeft():
+    try:
+        DRIVE_SYSTEM.go_bottom_left()
+    except Exception:
+        return Response('Failed to change the direction of the car!', status=500, mimetype='application/json')
+    
+    return Response('Successfully set direction of the car to bottom left!', status=200, mimetype='application/json')
+
+@CONTROLS_BP.route('/go-bottom-right', methods=['POST'])
+def driveBottomRight():
+    try:
+        DRIVE_SYSTEM.go_bottom_right()
+    except Exception:
+        return Response('Failed to change the direction of the car!', status=500, mimetype='application/json')
+    
+    return Response('Successfully set direction of the car to bottom right!', status=200, mimetype='application/json')
+
+@CONTROLS_BP.route('/turn-left', methods=['POST'])
+def turnLeft():
+    try:
+        DRIVE_SYSTEM.turn_left()
+    except Exception:
+        return Response('Failed to change the maneuver of the car to turn left!', status=500, mimetype='application/json')
+    
+    return Response('Successfully set maveuver of the car to turn left', status=200, mimetype='application/json')
+
+@CONTROLS_BP.route('/turn-right', methods=['POST'])
+def turnRight():
+    try:
+        DRIVE_SYSTEM.turn_right()
+    except Exception:
+        return Response('Failed to change the maneuver of the car to turn right!', status=500, mimetype='application/json')
+    
+    return Response('Successfully set maveuver of the car to turn right', status=200, mimetype='application/json')
