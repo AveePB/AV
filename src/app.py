058e47bb2096ca603ee508eace73e75467c35633
AveePB <mike303.py@gmail.com>
2025-04-22 00:38:27 +0200

    Updated consts LIDAR_DATA_PATH & LIDAR_USB_HEADER

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+from vehicle.consts import LIDAR_USB_HEADER, LIDAR_DATA_PATH
 from joystick import JOYSTICK_BP
 from routes import ROUTES_BP
 from car import Robot
@@ -12,7 +13,7 @@ if (__name__ == '__main__'):
     flask_process = Process(target=app.run)
     flask_process.start()
 
-    robot = Robot('test.csv', '/dev/ttyUSB0')
+    robot = Robot(LIDAR_DATA_PATH, LIDAR_USB_HEADER)
     robot_process = Process(target=robot.run)
     robot_process.start()
 
